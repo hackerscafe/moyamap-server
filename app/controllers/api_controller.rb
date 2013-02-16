637617009d4bdd9b6ee05f2cded6ac5c76b96955
Ohkubo KOHEI <ohkubo@magician.jp>
2013-02-16 13:08:48 +0900

    local wiki の api key 追加

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,7 +2,7 @@ require "open-uri"
 
 class ApiController < ApplicationController
   def call_wiki
-    res = open Configurable[:local_wiki_api_endpoint] + params[:path] + "?" + request.query_string
+    res = open Configurable[:local_wiki_api_endpoint] + params[:path] + "?api_key=#{Configurable[:local_wiki_api_key]}&" + request.query_string
     json = JSON.parse(res.read)
     render json: json
   end
