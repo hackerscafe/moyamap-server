849f1d8387db7ce20149ed0a5ee7901293d554b4
Ohkubo KOHEI <ohkubo@magician.jp>
2013-02-16 10:46:10 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoyaMap::Application.config.secret_token = '5b282ebf603466360ec6762f48fe2d1083f342ec3853b988a65345768d23490edce4d35f15c99eb4659e1b6c86f5339124e70d2abdbf018d85798657a459d702'
